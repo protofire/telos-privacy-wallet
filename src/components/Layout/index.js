e7e05729ef254798a00d55d1cc42cdbf06f1bbe0
0fprod <francisco.palacios@protofire.io>
2025-11-04 13:10:38 +0000

    Move menu to the left side

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
 
-export default ({ header, footer, children }) => (
-  <>
-    <Layout>
-      {header}
-      <PageContainer>
-        {children}
-      </PageContainer>
-      {footer}
-    </Layout>
-  </>
-);
+export default ({ header, footer, children }) => {
+  const [menu, content] = React.Children.toArray(children);
+  return (
+    <>
+      <Layout>
+        {header}
+        <PageContainer>
+          {menu}
+          <ContentContainer>
+            {content}
+          </ContentContainer>
+        </PageContainer>
+        {footer}
+      </Layout>
+    </>
+  );
+};
 
 const Layout = styled.div`
   flex: 1;
@@ -28,11 +34,23 @@ const Layout = styled.div`
 
 const PageContainer = styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: center;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: center;
   flex: 1;
+  gap: 32px;
   margin: 80px 0;
   position: relative;
+  @media only screen and (max-width: 560px) {
+    margin: 30px 0;
+  }
+`;
+
+const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
   @media only screen and (max-width: 560px) {
     margin: 30px 0;
   }
