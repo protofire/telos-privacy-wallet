3cb0a86c69db51e7541db410aef1381085c84b03
0fprod <francisco.palacios@protofire.io>
2025-11-06 15:52:24 +0000

    adjust page layout

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -23,24 +23,28 @@ export default ({ header, footer, children }) => {
 };
 
 const Layout = styled.div`
-  flex: 1;
+  min-height: 100vh;
   display: flex;
   flex-direction: column;
   box-sizing: border-box;
   padding: 14px 40px 40px;
+  justify-content: space-between;
+  gap: 40px;
   @media only screen and (max-width: 560px) {
     padding: 21px 7px 28px;
   }
   @media only screen and (max-width: 800px) {
     padding-bottom: 80px;
+  }
 `;
 
 const PageContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding-top: 40px;
-  margin: 0 auto 40px auto;
+  justify-content: center;
+  flex: 1;
+  margin: 0 auto;
 `;
 
 const MenuAndPageContainer = styled.div`
