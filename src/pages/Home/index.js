1b9b1b0eb5b539f5afe9c2b70f480338250e3b62
0fprod <francisco.palacios@protofire.io>
2025-11-07 11:54:45 +0000

    Add connect wallet home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -8,14 +8,16 @@ import Spinner from 'components/Spinner';
 import LatestTransactions from 'components/LatestTransactions';
 import Portfolio from 'components/Portfolio';
 import Link from 'components/Link';
+import Button from 'components/Button';
 
-import { PoolContext, ZkAccountContext, WalletContext } from 'contexts';
+import { PoolContext, ZkAccountContext, WalletContext, ModalContext } from 'contexts';
 
 export default () => {
   const { t } = useTranslation();
   const history = useHistory();
   const location = useLocation();
   const { address: account } = useContext(WalletContext);
+  const { openWalletModal } = useContext(ModalContext);
   const {
     history: fullHistory, zkAccount, pendingDirectDeposits,
     isLoadingZkAccount, isLoadingHistory,
@@ -33,10 +35,21 @@ export default () => {
 
   return (
     <ContentContainer>
-      {(account || zkAccount) && (
+      {account || zkAccount ? (
         <CardsContainer>
           <Card title={t('home.portfolio')}>
             <Portfolio />
+          </Card>
+        </CardsContainer>
+      ) : (
+        <CardsContainer>
+          <Card title={t('home.portfolio')}>
+            <EmptyPortfolioContainer>
+              <EmptyPortfolioText>{t('home.connectWalletToViewPortfolio')}</EmptyPortfolioText>
+              <ConnectWalletButton onClick={openWalletModal} data-ga-id="connect-wallet-home">
+                {t('buttonText.connectWallet')}
+              </ConnectWalletButton>
+            </EmptyPortfolioContainer>
           </Card>
         </CardsContainer>
       )}
@@ -103,8 +116,43 @@ const CardsContainer = styled.div`
 `;
 
 
+const EmptyPortfolioContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 24px;
+  gap: 16px;
+`;
+
+const EmptyPortfolioText = styled.span`
+  font-size: 14px;
+  color: ${props => props.theme.text.color.secondary};
+  text-align: center;
+  line-height: 1.5;
+`;
+
 const ContentContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 16px;
+`;
+
+const ConnectWalletButton = styled(Button)`
+  background: ${props => props.theme.color.telosGradientSoft};
+  color: rgb(149 126 223 / 90%);
+  border: 1px solid rgb(149 126 223 / 40%);
+  padding: 16px;
+  border-radius: 8px;
+  font-size: 16px;
+  cursor: pointer;
+  transition: all 0.3s ease;
+
+  &:hover {
+    background: ${props => props.theme.color.telosGradient};
+    color: ${props => props.theme.color.white};
+    transition: all 0.3s ease;
+    transform: scale(1.05);
+    box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.1);
+  }
 `;
