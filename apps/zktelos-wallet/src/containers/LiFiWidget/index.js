691f9463b7c2b2e12167add46c088d64f3852679
0fprod <francisco.palacios@protofire.io>
2026-01-08 12:59:10 +0000

    Enable lifi widget

--- apps/zktelos-wallet/src/containers/LiFiWidget/index.js
+++ apps/zktelos-wallet/src/containers/LiFiWidget/index.js
@@ -1,18 +1,20 @@
 import React, { useContext } from 'react';
 import { LiFiWidget } from '@lifi/widget';
+import { useTheme } from '@mui/material/styles';
 
 import { PoolContext } from 'contexts';
 import { useWindowDimensions } from 'hooks';
 
 export default () => {
   const { currentPool } = useContext(PoolContext);
+  const theme = useTheme();
   const { width } = useWindowDimensions();
 
   const widgetConfig = {
     integrator: 'zkTelos',
     fee: 0.00075,
     variant: 'compact',
-    appearance: 'light',
+    appearance: theme.palette.mode,
     disableAppearance: true,
     containerStyle: {
       width: width > 500 ? 480 : '100%',
@@ -35,6 +37,7 @@ export default () => {
       },
     },
     fromChain: 1,
+    // fromToken: '0x8D97Cea50351Fb4329d591682b148D43a0C3611b', // USDC Telos
     fromToken: '0xdAC17F958D2ee523a2206206994597C13D831ec7', // USDT Ethereum
     toChain: currentPool.chainId,
     toToken: currentPool.tokenAddress,
