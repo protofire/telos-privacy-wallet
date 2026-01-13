334e87493c7f8261774dd2289543d544e59184f9
0fprod <francisco.palacios@protofire.io>
2026-01-13 12:11:57 +0000

    Add support id and version to footer

--- apps/zktelos-wallet/src/components/Footer/index.js
+++ apps/zktelos-wallet/src/components/Footer/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import { useContext, createElement } from 'react';
 import styled from 'styled-components';
 import Link from 'components/Link';
+import appPackage from '../../../package.json';
 
 import { ReactComponent as TwitterIcon } from 'assets/twitter.svg';
 import { ReactComponent as TelegramIcon } from 'assets/telegram.svg';
 import { ReactComponent as MirrorIcon } from 'assets/mirror.svg';
 import { ReactComponent as GithubIcon } from 'assets/github.svg';
+import { SupportIdContext } from '../../contexts';
+import { useTranslation } from 'react-i18next';
 
 export default () => {
-
+  const { supportId } = useContext(SupportIdContext);
+  const { t } = useTranslation();
   const resources = [
     { icon: TwitterIcon, href: 'https://x.com/HelloTelos' },
     { icon: TelegramIcon, href: 'https://t.me/HelloTelos' },
@@ -22,9 +26,18 @@ export default () => {
         <InnerRow>
           {resources.map((resource, index) => (
             <CustomLink key={index} href={resource.href} target="">
-              {React.createElement(resource.icon, {})}
+              {createElement(resource.icon, {})}
             </CustomLink>
           ))}
+        </InnerRow>
+      </Row>
+      <Row>
+        <InnerRow>
+          <Text>v{appPackage.version}</Text>
+          <TextRow>
+            <Text style={{ marginRight: 4 }}>{t('common.supportId')}:</Text>
+            <Text>{supportId}</Text>
+          </TextRow>
         </InnerRow>
       </Row>
     </Column>
@@ -59,4 +72,18 @@ const CustomLink = styled(Link)`
   color: #A7A2B8;
   font-size: 14px;
   font-weight: ${props => props.theme.text.weight.bold};
+`;
+
+const Text = styled.span`
+  font-size: 14px;
+  color: #A7A2B8;
+  font-weight: ${props => props.theme.text.weight.bold};
+  line-height: 20px;
+  text-align: center;
+`;
+
+const TextRow = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
 `;
